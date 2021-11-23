3c9afed7cbf7b405c0dee5a379c4cfb4c82ba0a5
Vikas Kapadiya <vikas5914@users.noreply.github.com>
2021-11-23 21:02:27 +0000

    Apply fixes from StyleCI

--- app/main.php
+++ app/main.php
@@ -1,6 +1,6 @@
 <?php
 
-require __DIR__ . '/../vendor/autoload.php';
+require __DIR__.'/../vendor/autoload.php';
 
 use Symfony\Component\HttpClient\HttpClient;
 
@@ -17,10 +17,10 @@ try {
 
     $client = HttpClient::create([
         'headers' => [
-            'accept' => 'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-            'user-agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36',
+            'accept'        => 'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'user-agent'    => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36',
             'cache-control' => 'no-cache',
-        ]
+        ],
     ]);
 
     $response = $client->request('GET', $url);
